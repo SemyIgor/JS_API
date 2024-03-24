6cc81a5e4aabcd8690f8bdf02e64daf258f25c56
HenkSeed <adaner@inbox.ru>
2024-03-24 18:45:24 +0700

    Result Slider on the Seminar02_HW

--- Seminar02_HW/scriptnew.js
+++ Seminar02_HW/scriptnew.js
@@ -15,16 +15,16 @@
  */
 
 const catsImgs = [
-	// './img/image00.jpg',
-	// './img/image01.jpg',
-	// './img/image02.jpg',
-	// './img/image03.jpg',
-	// './img/image04.jpg',
-	'https://masterpiecer-images.s3.yandex.net/f616c1126d3311eea1474659bdca6a39:upscaled',
-	'https://masterpiecer-images.s3.yandex.net/e57dcdfa6acc11ee8e2992669a1675b3:upscaled',
-	'https://masterpiecer-images.s3.yandex.net/b0dff703878211ee9c683abd0be4d755:upscaled',
-	'https://masterpiecer-images.s3.yandex.net/8e120d2a9d9111ee8d617a83974e0feb:upscaled',
-	'https://avatars.mds.yandex.net/get-shedevrum/9283310/b133a531b24311eeb2ebd6f07e64960d/orig',
+	'./img/image00.jpg',
+	'./img/image01.jpg',
+	'./img/image02.jpg',
+	'./img/image03.jpg',
+	'./img/image04.jpg',
+	// 'https://masterpiecer-images.s3.yandex.net/f616c1126d3311eea1474659bdca6a39:upscaled',
+	// 'https://masterpiecer-images.s3.yandex.net/e57dcdfa6acc11ee8e2992669a1675b3:upscaled',
+	// 'https://masterpiecer-images.s3.yandex.net/b0dff703878211ee9c683abd0be4d755:upscaled',
+	// 'https://masterpiecer-images.s3.yandex.net/8e120d2a9d9111ee8d617a83974e0feb:upscaled',
+	// 'https://avatars.mds.yandex.net/get-shedevrum/9283310/b133a531b24311eeb2ebd6f07e64960d/orig',
 ];
 
 const arrowRight = document.querySelector('.arrow-right path');
@@ -43,8 +43,8 @@ let imageIndexAfter;
 neighborIndexes(imageIndex);
 
 // Навешиваем события на кнопки слайдера
-arrowRight.addEventListener('click', () => moveRight());
-arrowLeft.addEventListener('click', () => moveLeft());
+arrowRight.addEventListener('click', () => moveLeft());
+arrowLeft.addEventListener('click', () => moveRight());
 
 // Выводим в DOM тройку картинок и пагинацию
 putInDOMImagesSet();
@@ -66,37 +66,43 @@ function clearCurrentPagination() {
 	});
 }
 
-// ??????
 function paginEventActivate(event) {
 	const pagIndex = parseInt(event.target.dataset.pag);
+	const compareIndexes = pagIndex - imageIndex;
 	// Если кликнули не по номеру картинки, которая уже на экране
-	if (pagIndex != imageIndex) {
+	// (Иначе ничего не происходит)
+	if (compareIndexes != 0) {
+		// Удаляем признак текущей пагинации (сбрасываем точки)
 		clearCurrentPagination();
 
 		// Делаем "текущим" элемент пагинации, на который кликнули
-		event.target.classList.add('pagin-item_current');
-
-		// Определяем индексы новой тройки картинок
 		imageIndex = pagIndex;
-		neighborIndexes(imageIndex);
-
-		// Заменяем в DOM-е первую картинку в тройке на выбранную по номеру пагинации
-		imagesQueue.firstElementChild.attributes.src.value = catsImgs[imageIndex];
-
-		// Плавный скроллинг всей тройки картинок вправо, выбранная картинка "выплывает" на экран
-		imagesQueueMoveRight();
 		paginationSet[imageIndex].classList.add('pagin-item_current');
 
-		// Выводим картинку во фрейм и "поднимаем" фрейм над "плёнкой"
-		refreshFrame();
-
-		// Под прикрытием фрейма (там наша картинка) двигаем влево нашу плёнку (сокращённый вариант)
-		setTimeout(returnLeft, 500);
-
-		// Вычисляем индексы новой тройки картинок, которые далее загрузим в "плёнку"
+		// Определяем индексы новой тройки картинок !!!!
 		neighborIndexes(imageIndex);
-		// и загружаем новые картинки на "плёнку"
-		putInDOMImagesSet();
+
+		if (compareIndexes > 0) {
+			// Заменяем в "паровозике" последнюю картинку в тройке на выбранную по номеру пагинации
+			imagesQueue.lastElementChild.attributes.src.value = catsImgs[imageIndex];
+
+			// Плавный скроллинг всей тройки картинок влево, выбранная картинка "выплывает" на экран
+			imagesQueueMoveLeft();
+
+			setTimeout(imagesQueueReturnRight, 500);
+			// Загружаем в DOM новую тройку картинок
+			setTimeout(putInDOMImagesSet, 500);
+		} else {
+			// Заменяем в "паровозике" первую картинку в тройке на выбранную по номеру пагинации
+			imagesQueue.firstElementChild.attributes.src.value = catsImgs[imageIndex];
+
+			// Плавный скроллинг всей тройки картинок вправо, выбранная картинка "выплывает" на экран
+			imagesQueueMoveRight();
+
+			setTimeout(imagesQueueReturnLeft, 500);
+			// Загружаем в DOM новую тройку картинок
+			setTimeout(putInDOMImagesSet, 500);
+		}
 	}
 }
 
@@ -137,23 +143,33 @@ function putInDOMImagesSet() {
 	);
 }
 
-// Функция вывода во фрейм текущей картинки (поверх такой же в "паровозике")
-// и поднятия фрейма над "паровозиком", скрывая его видимую (текущую) картинку
+// Функция помещения во фрейм текущей картинки
 function refreshFrame() {
 	frameSet.innerHTML = `<img class="frame-img" src="${catsImgs[imageIndex]}" alt="cats">`;
-	frameSet.setAttribute('style', 'z-index: 10');
+	// frameSet.setAttribute('style', 'z-index: 10');
 }
 
 // Функция скроллинга вправо (основная)
 function moveRight() {
-	imagesQueueMoveRight();
-	// // Делаем "текущим" элемент пагинации, который соответствует новой картинке
-	paginationSet[imageIndexBefore].classList.add('pagin-item_current');
-	// Определяем новые три картинки, которые заместят собой предыдущие
+	// Удаляем признак текущей пагинации (сбрасываем точки)
+	clearCurrentPagination();
 
+	// Определяем индекс следующей картинки
 	imageIndex = imageIndex - 1 < 0 ? catsImgs.length - 1 : imageIndex - 1;
+
+	// Определяем индексы новой тройки картинок !!!!
 	neighborIndexes(imageIndex);
 
+	// Делаем "текущим" элемент пагинации, который соответствует новой картинке
+	paginationSet[imageIndex].classList.add('pagin-item_current');
+
+	// Заменяем в "паровозике" первую картинку в тройке на выбранную по номеру пагинации
+	imagesQueue.firstElementChild.attributes.src.value = catsImgs[imageIndex];
+
+	// Плавный скроллинг всей тройки картинок вправо, выбранная картинка "выплывает" на экран
+	imagesQueueMoveRight();
+
+	// Возвращаем на место (влево) "паровозик"
 	setTimeout(imagesQueueReturnLeft, 500);
 	// Загружаем в DOM новую тройку картинок
 	setTimeout(putInDOMImagesSet, 500);
@@ -161,21 +177,50 @@ function moveRight() {
 
 // Функция сдвига трёх картинок, загруженных в DOM, вправо
 function imagesQueueMoveRight() {
-	// Убираем "прикрывающую" картинку (фрейм) за слайдер
-	frameSet.setAttribute('style', 'z-index: -10');
-	// Плавно перемещаем три картинки, находящиеся в DOM-е, вправо
-	imagesQueue.classList.add('images-rightmove');
-	imagesQueue.classList.remove('images-queueinitial');
+	// Убираем фрейм за слайдер
+	frameDown();
 
-	clearCurrentPagination();
+	// Плавно перемещаем три картинки, находящиеся в DOM-е, вправо
+	queueMoveRight();
+
+	// // Выводим картинку во фрейм
+	refreshFrame();
+
+	// Размещаем фрейм перед слайдером
+	setTimeout(frameUp, 500);
+
+	// clearCurrentPagination();
+}
+
+// Ставим "прикрывающую картинку" (фрейм) перед слайдером
+function frameUp() {
+	frameSet.setAttribute('style', 'z-index: 10');
+}
+
+// Убираем "прикрывающую" картинку (фрейм) за слайдер
+function frameDown() {
+	console.log('frameSet: ', frameSet);
+	frameSet.setAttribute('style', 'z-index: -10');
+}
+
+// Сдвигаем "паровозик" вправо
+function queueMoveRight() {
+	// Плавно перемещаем три картинки, находящиеся в DOM-е, вправо
+	imagesQueue.classList.remove('images-leftmove');
+	imagesQueue.classList.remove('images-queueinitial');
+	imagesQueue.classList.add('images-rightmove');
+}
+
+// Сдвигаем "паровозик" влево
+function queueMoveLeft() {
+	// Плавно перемещаем три картинки, находящиеся в DOM-е, влево
+	imagesQueue.classList.remove('images-rightmove');
+	imagesQueue.classList.remove('images-queueinitial');
+	imagesQueue.classList.add('images-leftmove');
 }
 
 // Функция возврата трёх картинок, загруженных в DOM, влево
 function imagesQueueReturnLeft() {
-	// Загружаем во фрейм копию той картинки, которая пришла на экран слайдера и
-	// поднимаем фрейм для маскировки возвращения новой тройки картинок на исходную
-	refreshFrame();
-
 	// Возвращаем тройку картинок на исходную под прикрытием фрейма
 	returnLeft();
 }
@@ -184,36 +229,68 @@ function imagesQueueReturnLeft() {
 function returnLeft() {
 	// Возвращаем тройку картинок на исходную под прикрытием фрейма
 	imagesQueue.classList.remove('images-rightmove');
+	// imagesQueue.classList.remove('images-leftmove');
 	imagesQueue.classList.add('images-queueinitial');
 }
 
 // Функция скроллинга влево (основная)
 function moveLeft() {
+	// Удаляем признак текущей пагинации (сбрасываем точки)
+	clearCurrentPagination();
+
+	console.log('imageIndex: ', imageIndex);
+	// Определяем индекс следующей картинки
+	imageIndex = imageIndex + 1 > catsImgs.length - 1 ? 0 : imageIndex + 1;
+
+	// Определяем индексы новой тройки картинок !!!!
+	neighborIndexes(imageIndex);
+	console.log('imageIndexBefore: ', imageIndexBefore);
+	console.log('imageIndex: ', imageIndex);
+	console.log('imageIndexAfter: ', imageIndexAfter);
+
+	console.log('paginationSet[imageIndex]: ', paginationSet[imageIndex]);
+	// Делаем "текущим" элемент пагинации, который соответствует новой картинке
+	paginationSet[imageIndex].classList.add('pagin-item_current');
+	console.log('paginationSet: ', paginationSet);
+
+	// Заменяем в "паровозике" последнюю картинку в тройке на выбранную по номеру пагинации
+	imagesQueue.lastElementChild.attributes.src.value = catsImgs[imageIndex];
+
+	// Плавный скроллинг всей тройки картинок влево, выбранная картинка "выплывает" на экран
 	imagesQueueMoveLeft();
+
+	// Возвращаем на место (влево) "паровозик"
 	setTimeout(imagesQueueReturnRight, 500);
+
+	// Загружаем в DOM новую тройку картинок
 	setTimeout(putInDOMImagesSet, 500);
 }
 
 // Функция сдвига трёх картинок, загруженных в DOM, влево
 function imagesQueueMoveLeft() {
-	// Убираем "прикрывающую" картинку (фрейм) за слайдер
-	frameSet.setAttribute('style', 'z-index: -10');
+	// Убираем фрейм за слайдер
+	frameDown();
+
 	// Плавно перемещаем три картинки, находящиеся в DOM-е, вправо
-	imagesQueue.classList.add('images-leftmove');
-	imagesQueue.classList.remove('images-queueinitial');
+	queueMoveLeft();
 
-	// Определяем новые три картинки, которые заместят собой предыдущие
-	imageIndex = imageIndex + 1 >= catsImgs.length ? 0 : imageIndex + 1;
-	neighborIndexes(imageIndex);
+	// // Выводим картинку во фрейм
+	refreshFrame();
 
-	clearCurrentPagination();
-	// Делаем "текущим" элемент пагинации, который соответствует новой картинке
-	paginationSet[imageIndex].classList.add('pagin-item_current');
+	// Размещаем фрейм перед слайдером
+	setTimeout(frameUp, 500);
 }
 
 // Функция возврата трёх картинок, загруженных в DOM, вправо
 function imagesQueueReturnRight() {
-	refreshFrame();
+	// Возвращаем тройку картинок на исходную под прикрытием фрейма
+	returnRight();
+}
+
+// Функция возврата тройки картинок на исходную
+function returnRight() {
+	// Возвращаем тройку картинок на исходную под прикрытием фрейма
 	imagesQueue.classList.remove('images-leftmove');
+	// imagesQueue.classList.remove('images-rightmove');
 	imagesQueue.classList.add('images-queueinitial');
 }
